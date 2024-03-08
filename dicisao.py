9374b1f732bf84cf4bb404d76542fd5220fd7460
RAFAEL VIEIRA BRANDES <202402637916@alunos.ibmec.edu.br>
2024-03-08 11:36:45 -0300

    ac4 feita

---
+++ dicisao.py
@@ -0,0 +1,19 @@
+"""estruturas de decisao
+- if/ else/ elif
+-mach/case
+"""
+
+def saudacao(turno):
+    if turno =="M"
+        print ("bom dia")
+    elif turno == "I" : # esle if
+        print ("boa tarde")
+    elif turno == "N" :
+        print ("boa noite")
+    else:
+        print ("dado invalido")
+
+def saudacao2(turno):
+    if turno =="M":
+        return "bom dia"
+    elif turno 
